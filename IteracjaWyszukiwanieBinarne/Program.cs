170374056de6ee8b0f6adf8a3cb2ed1093379c45
krystianpetek <krystianpetek2@gmail.com>
2021-07-15 11:03:56 +0200

    WyszukiwanieBinarne

---
+++ IteracjaWyszukiwanieBinarne/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace IteracjaWyszukiwanieBinarne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę aby sprawdzić czy należy do przedziału");
+            int liczba = int.Parse(Console.ReadLine());
+            int[] tablicaLiczb = new int[] { 10, 11, 60, 70, 90, 90, 100 };
+            int wynik = WyszukiwanieBinarne(tablicaLiczb,liczba);
+            if(wynik == -1)
+                Console.WriteLine("Nie znaleziono liczby");
+            else
+                Console.WriteLine("Wynik jest na pozycji " + wynik);
+        }
+        static int WyszukiwanieBinarne(int[] tablicaLiczb,int liczba)
+        {
+            Array.Sort(tablicaLiczb);
+
+            int lewo = 0;
+            int prawo = tablicaLiczb.Length - 1;
+            int szukanyIndeks;
+
+            while(lewo <=prawo)
+            {
+                szukanyIndeks = (lewo+ prawo) / 2;
+
+                if (tablicaLiczb[szukanyIndeks] == liczba)
+                    return szukanyIndeks;
+
+                if(tablicaLiczb[szukanyIndeks] > liczba)
+                    prawo = szukanyIndeks - 1;
+                else
+                    lewo = szukanyIndeks + 1;
+            }
+            return -1;
+        }
+    }
+}
