1bbc5ac30ce21efe480701118f97294cbfcd500c
Krystian Petek <krystianpetek2@gmail.com>
2022-02-14 23:06:13 +0100

    upod

--- 11_SredniaArytmetyczna/Program.cs
+++ 11_SredniaArytmetyczna/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Średnia arytmetyczna");
+double wynik = 0;
+Console.Write("Podaj ilość liczb: ");
+int.TryParse(Console.ReadLine(), out int n);
+int i = 0;
+while (i < n)
+{
+    Console.Write("Podaj liczbę: ");
+    int.TryParse(Console.ReadLine(), out int a);
+    Console.Write($"{wynik} + {a} = ");
+    wynik += a;
+    Console.WriteLine($"{wynik}");
+    i++;
+}
+Console.Write($"Średnia arytmetyczna: {wynik} / {n} = ");
+wynik = wynik / n;
+Console.Write($"{wynik}");
---
+++ 12_TestParzystosci/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("TEST PARZYSTOŚCI");
+Console.Write("Podaj liczbę parzystą: ");
+string? wejscie = Console.ReadLine();
+bool sprawdzParzystosc = int.TryParse(wejscie, out int parzysta);
+if (!sprawdzParzystosc)
+{
+    Console.WriteLine("Niepoprawne dane wejściowe.");
+    return;
+}
+string wynik = (parzysta % 2 == 0) ? $"Liczba {parzysta} jest parzysta" : $"Liczba {parzysta} nie jest parzysta";
+Console.WriteLine(wynik);
