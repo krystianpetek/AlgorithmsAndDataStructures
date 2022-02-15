94ab07e4dfb22d6bcaf953f56c1ce7f8befd9ef7
Krystian Petek <krystianpetek2@gmail.com>
2022-02-15 14:51:31 +0100

    up

--- 1_SortowanieBabelkowe/Program.cs
+++ 1_SortowanieBabelkowe/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿int[] tablicaLiczb = { 1000, 33, 123, 10, 1, 12442, 99, 111 };
+Console.Write($"Tablica liczb przed sortowaniem: [");
+Wypisz(tablicaLiczb);
+var tablicaLiczbPoSortowaniu = Sortowanie(tablicaLiczb);
+Console.Write($"Tablica liczb po sortowaniu: [");
+Wypisz(tablicaLiczbPoSortowaniu);
+
+int[] Sortowanie(int[] tablica)
+{
+    bool flaga = true;
+    int licznik = 0;
+    int licznikDrugi = ++licznik;
+    while(flaga)
+    {
+        if(tablica[licznik] > tablica[licznik+1])
+        {
+            int temp = tablica[licznik + 1];
+            tablica[licznikDrugi] = tablica[licznik];
+            tablica[licznik] = temp;
+        }
+    }
+}
+
+void Wypisz(int[] tablicaLiczb)
+{
+    for (int i = 0; i < tablicaLiczb.Length; i++)
+    {
+        if (i == tablicaLiczb.Length - 1)
+            Console.Write($"{tablicaLiczb[i]}");
+        else
+            Console.Write($"{tablicaLiczb[i]}, ");
+    }
+    Console.Write("]\n");
+}
+// http://www.algorytm.org/algorytmy-sortowania/sortowanie-babelkowe-bubblesort.html
