fe9f775af348ef53ac6ec01c2b332dfc5fc13249
Krystian Petek <krystianpetek2@gmail.com>
2022-02-17 19:48:01 +0100

    rekur

---
+++ 1_SumowanieRekurencyjne/Program.cs
@@ -0,0 +1,11 @@
+﻿int SumowanieRekurencja(int a, int b)
+{
+    if (a == 0)
+        return b;
+    else
+    {
+        return SumowanieRekurencja(--a, ++b);
+    }
+}
+
+Console.WriteLine(SumowanieRekurencja(1000, 600));
---
+++ 2_SilniaRekurencyjna/Program.cs
@@ -0,0 +1,8 @@
+﻿int Silnia(int n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return n * Silnia(n - 1);
+}
+Console.WriteLine(Silnia(5));
