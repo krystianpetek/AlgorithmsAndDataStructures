2b31718efffb2246b8b7b98cdd327fac2c1cfb3b
Krystian Petek <krystianpetek2@gmail.com>
2022-02-19 05:56:57 +0100

    Update Program.cs

--- 5_SortowaniePrzezKopcowanie/Program.cs
+++ 5_SortowaniePrzezKopcowanie/Program.cs
@@ -1,13 +1,37 @@
-﻿int[] tablicaLiczb = { 1000, 33, 123, 10, 1, 12442, 99, 111 };
+﻿int[] tablicaLiczb = {0, 1000, 33, 123, 10, 1, 12442, 99, 111 };
+tablicaLiczb = new[] {0, 1000, 33, 123, 10, 1, 12442, 99, 111 };
 Console.Write($"Tablica liczb przed sortowaniem: [");
 Wypisz(tablicaLiczb);
-var tablicaLiczbPoSortowaniu = Sortowanie(tablicaLiczb, 0, tablicaLiczb.Length - 1);
+var tablicaLiczbPoSortowaniu = Sortowanie(tablicaLiczb);
 Console.Write($"Tablica liczb po sortowaniu: [");
 Wypisz(tablicaLiczbPoSortowaniu);
 
-int[] Sortowanie(int[] tablica, int lewo, int prawo)
+int[] Sortowanie(int[] tablica)
 {
+    // budowanie kopca
+    for (int i = 2; i < tablica.Length; i++)
+    {
+        int rodzic = i / 2;
+        int lisc = i;
+        while(rodzic > 0 && tablica[lisc] > tablica[rodzic])
+        {
+            Zamiana(ref tablica[lisc], ref tablica[rodzic]);
+            lisc = rodzic;
+            rodzic = lisc / 2;
+        }
+    }
 
+    // rozbiorka kopca
+    for (int i = tablica.Length - 1; i > 1; i--)
+    {
+        Zamiana(ref tablica[1], ref tablica[i]);
+        int korzen = 1;
+        int rodzic = 2;
+        while(rodzic < korzen)
+        {
+            if (rodzic + 1 < i && tablica[rodzic+1] > tablica[rodzic])
+        }
+    }
     return tablica;
 }
 
