9ae6fb1400e280aa2758818ce3dad6d5e2b5ba5e
Krystian Petek <krystianpetek2@gmail.com>
2022-10-23 15:43:12 +0200

    WSEI Lecturer - SPOJ task - NPC2014F completed

---
+++ WSEI_AlgorithmsAndDataStructures/NPC2014F - Field/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+static class Field
+{
+    static void Main()
+    {
+        var plants = new int[26];
+        var blockOfTiles = new int[26];
+
+        long.TryParse(Console.ReadLine(), out long N);
+        var letters = Console.ReadLine();
+        long.TryParse(Console.ReadLine(), out long K);
+
+        for (int i = 0; i < K; i++)
+        {
+            var lineK = Console.ReadLine().Split(" ");
+
+            int Xi = int.Parse(lineK[0]);
+            char Yi = char.Parse(lineK[1]);
+
+            plants[Yi - 'a'] = Xi;
+        }
+
+        var left = 0;
+        var right = 0;
+        var counter = int.MaxValue;
+        var windowCount = 0;
+
+        while (left < N && right < N)
+        {
+            var letterPositionLeft = letters[right] - 'a';
+            blockOfTiles[letterPositionLeft]++;
+
+            if (blockOfTiles[letterPositionLeft] == plants[letterPositionLeft])
+                windowCount++;
+
+            if (windowCount == K)
+            {
+                while (blockOfTiles[letters[left] - 'a'] > plants[letters[left] - 'a'])
+                {
+                    left++;
+                    blockOfTiles[letters[left - 1] - 'a']--;
+                }
+                counter = (right - left + 1 < counter) ? right - left + 1 : counter;
+            }
+            right++;
+        }
+
+        if (counter != int.MaxValue)
+            Console.WriteLine(counter);
+        else
+            Console.WriteLine("Andy rapopo");
+    }
+}
