c37fce5ff17ed808f41f3076618c8972bd6cb461
Krystian Petek <krystianpetek2@gmail.com>
2022-11-19 22:15:33 +0100

    WSEI Lecturer - SPOJ task - STPAR - Street Parade completed (Queue and Stack)

---
+++ WSEI_AlgorithmsAndDataStructures/STPAR - Street Parade/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+public class StreetParade
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            if (!int.TryParse(Console.ReadLine(), out int N))
+                return;
+
+            if (N == 0)
+                return;
+
+            var line = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var parade = Array.ConvertAll<string, int>(line, int.Parse);
+            var queue = new Queue<int>(parade);
+            var lane = new Stack<int>();
+
+            var headOfParade = 1;
+            while (queue.Count > 0 || lane.Count > 0)
+            {
+
+                if (lane.Count > 0 && lane.Peek() == headOfParade)
+                {
+                    lane.Pop();
+                    headOfParade++;
+                    continue;
+                }
+
+                if (queue.Count > 0 && queue.Peek() == headOfParade)
+                {
+                    queue.Dequeue();
+                    headOfParade++;
+                    continue;
+                }
+
+                if (queue.Count > 0 && queue.Peek() > headOfParade)
+                {
+                    lane.Push(queue.Dequeue());
+                    continue;
+                }
+
+                Console.WriteLine("no");
+                break;
+            }
+
+            if (queue.Count == 0 && lane.Count == 0)
+            {
+                Console.WriteLine("yes");
+            }
+        }
+    }
+}
