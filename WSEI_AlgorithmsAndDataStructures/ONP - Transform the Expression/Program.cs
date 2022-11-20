3b9b15f4fc818bcea957cf73cda1fad89241397a
Krystian Petek <krystianpetek2@gmail.com>
2022-11-20 05:53:29 +0100

    WSEI Lecturer - SPOJ task - ONP - Transform the Expression completed

---
+++ WSEI_AlgorithmsAndDataStructures/ONP - Transform the Expression/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class ONP
+{
+    public static void Main()
+    {
+        if (!int.TryParse(Console.ReadLine(), out int t))
+            return;
+
+        while(t > 0)
+        {
+            t--;
+            var expression = Console.ReadLine();
+            
+            int iterator = 0;
+            Stack<char> stack = new Stack<char>();
+            var output = new StringBuilder();
+
+            while(iterator < expression.Length - 1) 
+            {
+                iterator++;
+                switch(expression[iterator])
+                {
+                    case '(':
+                        break;
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '^':
+                        stack.Push(expression[iterator]);
+                        break;
+                    case ')':
+                        if(stack.Count > 0)
+                            output.Append(stack.Pop());
+                        break;
+                    default:
+                        output.Append(expression[iterator]);
+                        break;
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
