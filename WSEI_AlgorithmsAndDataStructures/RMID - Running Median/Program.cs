9684d6a8b9867e862745819c480a098abe82be7d
Krystian Petek <krystianpetek2@gmail.com>
2022-12-26 10:01:35 +0100

    RMID rewrite

--- WSEI_AlgorithmsAndDataStructures/RMID - Running Median/Program.cs
+++ WSEI_AlgorithmsAndDataStructures/RMID - Running Median/Program.cs
@@ -1,121 +1,191 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+
+//internal class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        StringBuilder stringBuiler = new StringBuilder();
+//        List<int> list = new List<int>();
+//        bool flag = false;
+
+//        while (true)
+//        {
+//            int readline = FastIO.ReadInt();
+//            //string readLine1 = Console.ReadLine();
+//            //if (string.IsNullOrEmpty(readLine1)) break;
+//            //var readline = int.Parse(readLine1);
+
+//            if (readline == 13 || readline == 10 || readline == 32)
+//            {
+//                if (flag)
+//                    break;
+
+//                flag = true;
+//                continue;
+//            }
+//            else
+//                flag = false;
+
+//            if (readline == -1)
+//            {
+//                //Calculate(list);
+//                if (list.Count % 2 == 0)
+//                {
+//                    stringBuiler.AppendLine(list[(list.Count / 2) - 1].ToString());
+//                    list.RemoveAt((list.Count / 2) - 1);
+//                }
+//                else
+//                {
+//                    stringBuiler.AppendLine(list[(list.Count / 2)].ToString());
+//                    list.RemoveAt((list.Count / 2));
+//                }
+//                continue;
+//            }
+
+//            if (readline == 0)
+//            {
+//                stringBuiler.AppendLine();
+//                list.Clear();
+//                _ = Console.ReadLine();
+
+//                continue;
+//            }
+
+//            list.Add(readline);
+//        }
+//        Console.WriteLine(stringBuiler.ToString().Trim());
+//    }
+//    static void Calculate(List<int?> list) => list.ForEach(x => { Console.Write($"{x} "); });
+//}
+
+//public static class FastIO
+//{
+//    private const byte _null = (byte)'\0';
+//    private const byte _minusSign = (byte)'-';
+//    private const byte _zero = (byte)'0';
+//    private const int _inputBufferLimit = 8192;
+
+//    private static readonly Stream _inputStream = Console.OpenStandardInput();
+//    private static readonly byte[] _inputBuffer = new byte[_inputBufferLimit];
+//    private static int _inputBufferSize = 0;
+//    private static int _inputBufferIndex = 0;
+
+//    private static byte ReadByte()
+//    {
+//        if (_inputBufferIndex == _inputBufferSize)
+//        {
+//            _inputBufferIndex = 0;
+//            _inputBufferSize = _inputStream.Read(_inputBuffer, 0, _inputBufferLimit);
+//            if (_inputBufferSize == 0)
+//                return _null;
+//        }
+
+//        return _inputBuffer[_inputBufferIndex++];
+//    }
+
+//    public static int ReadInt()
+//    {
+//        byte digit;
+//        do
+//        {
+//            digit = ReadByte();
+//            if (digit == 13 || digit == 32)
+//            {
+//                //digit = ReadByte();
+//                //if (digit == 13)
+//                    return digit;
+//            }
+//        }
+//        while (digit < _minusSign);
+
+//        bool isNegative = digit == _minusSign;
+//        if (isNegative)
+//        {
+//            digit = ReadByte();
+//        }
+
+//        int result = isNegative ? -(digit - _zero) : (digit - _zero);
+//        while (true)
+//        {
+//            digit = ReadByte();
+//            if (digit < _zero) break;
+//            result = result * 10 + (isNegative ? -(digit - _zero) : (digit - _zero));
+//        }
+
+//        return result;
+//    }
+//}
+
+using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 
-internal class Program
+namespace RunningMedian
 {
-    static void Main(string[] args)
+    class Program
     {
-        StringBuilder stringBuiler = new StringBuilder();
-        List<int> list = new List<int>();
-        bool flag = false;
-
-        while (true)
+        static void Main(string[] args)
         {
-            int readline = FastIO.ReadInt();
-            //string readLine1 = Console.ReadLine();
-            //if (string.IsNullOrEmpty(readLine1)) break;
-            //var readline = int.Parse(readLine1);
+            StackAndQueue();
+        }
 
-            if (readline == 13 || readline == 10 || readline == 32)
+        private static void StackAndQueue()
+        {
+            var stack = new Stack<int>();
+            var queue = new Queue<int>();
+            string input;
+            var sb = new StringBuilder();
+            do
             {
-                if (flag)
-                    break;
-
-                flag = true;
-                continue;
-            }
-            else
-                flag = false;
-
-            if (readline == -1)
-            {
-                //Calculate(list);
-                if (list.Count % 2 == 0)
+                input = Console.ReadLine();
+                // END OF FILE
+                if (input == null || input == "eof") break;
+                // 0
+                if (input == "0")
                 {
-                    stringBuiler.AppendLine(list[(list.Count / 2) - 1].ToString());
-                    list.RemoveAt((list.Count / 2) - 1);
+                    Console.ReadLine();
+                    sb.AppendLine();
+                    queue.Clear();
+                    stack.Clear();
+                    continue;
+                }
+
+                var num = int.Parse(input);
+                if (num == -1)
+                {
+                    sb.AppendLine(stack.Pop().ToString());
+
+                    // POP
+                    if (stack.Count < queue.Count)
+                    {
+                        stack.Push(queue.Dequeue());
+                    }
                 }
                 else
                 {
-                    stringBuiler.AppendLine(list[(list.Count / 2)].ToString());
-                    list.RemoveAt((list.Count / 2));
+                    if (queue.Count > 0)
+                    {
+                        if (queue.Count == stack.Count)
+                            stack.Push(queue.Dequeue());
+                        queue.Enqueue(num);
+                    }
+                    else if (stack.Count == 0)
+                    {
+                        stack.Push(num);
+                    }
+                    else if (stack.Count == 1)
+                    {
+                        queue.Enqueue(num);
+                    }
                 }
-                continue;
-            }
+            } while (true);
 
-            if (readline == 0)
-            {
-                stringBuiler.AppendLine();
-                list.Clear();
-                _ = Console.ReadLine();
-
-                continue;
-            }
-
-            list.Add(readline);
+            Console.Write(sb.ToString());
         }
-        Console.WriteLine(stringBuiler.ToString().Trim());
-    }
-    static void Calculate(List<int?> list) => list.ForEach(x => { Console.Write($"{x} "); });
-}
-
-public static class FastIO
-{
-    private const byte _null = (byte)'\0';
-    private const byte _minusSign = (byte)'-';
-    private const byte _zero = (byte)'0';
-    private const int _inputBufferLimit = 8192;
-
-    private static readonly Stream _inputStream = Console.OpenStandardInput();
-    private static readonly byte[] _inputBuffer = new byte[_inputBufferLimit];
-    private static int _inputBufferSize = 0;
-    private static int _inputBufferIndex = 0;
-
-    private static byte ReadByte()
-    {
-        if (_inputBufferIndex == _inputBufferSize)
-        {
-            _inputBufferIndex = 0;
-            _inputBufferSize = _inputStream.Read(_inputBuffer, 0, _inputBufferLimit);
-            if (_inputBufferSize == 0)
-                return _null;
-        }
-
-        return _inputBuffer[_inputBufferIndex++];
-    }
-
-    public static int ReadInt()
-    {
-        byte digit;
-        do
-        {
-            digit = ReadByte();
-            if (digit == 13 || digit == 32)
-            {
-                //digit = ReadByte();
-                //if (digit == 13)
-                    return digit;
-            }
-        }
-        while (digit < _minusSign);
-
-        bool isNegative = digit == _minusSign;
-        if (isNegative)
-        {
-            digit = ReadByte();
-        }
-
-        int result = isNegative ? -(digit - _zero) : (digit - _zero);
-        while (true)
-        {
-            digit = ReadByte();
-            if (digit < _zero) break;
-            result = result * 10 + (isNegative ? -(digit - _zero) : (digit - _zero));
-        }
-
-        return result;
     }
 }
